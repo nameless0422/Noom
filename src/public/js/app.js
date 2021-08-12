a8df0d59c0e683c2bf5eadc28c171ebe10baef53
nameless0422 <cjy042218@gmail.com>
2021-08-12 14:03:25 +0900

    finished chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,16 +2,78 @@ const socket = io();
 
 const welcome = document.querySelector("#welcome");
 const form = welcome.querySelector("form");
+const room = document.querySelector("#room");
 
-function backendDone(){
-    console.log("backend done");
+room.hidden = true;
+
+let roomName;
+
+function addMessage(message){
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+function showRoom(){
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+    msgForm.addEventListener("submit",handleMessageSubmit);
+    nameForm.addEventListener("submit",handleNicknameSubmit);
+}
+
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    socket.emit("new_message", input.value, roomName, ()=>{
+        addMessage(`You: ${input.value}`);
+        input.value = "";
+    });
+}
+
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    socket.emit("nickname", input.value);
 }
 
 function handleRoomSubmit(event){
     event.preventDefault();
     const input = form.querySelector("input");
-    socket.emit("enter_room", input.value, backendDone);
+    socket.emit("enter_room", input.value, showRoom);
+    roomName = input.value;
     input.value = "";
 }
 
 form.addEventListener("submit",handleRoomSubmit);
+
+socket.on("welcome",(user,newCount)=>{
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
+    addMessage(`Welcome ${user}!`);
+});
+
+socket.on("bye",(user,newCount)=>{
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
+    addMessage(`${user} left!`);
+});
+
+socket.on("new_message",addMessage);
+
+socket.on("room_change",(rooms)=>{
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML = "";
+    if(rooms.length === 0){
+        return;
+    }
+    rooms.forEach(room => {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.append(li);
+    });
+});
--- src/server.js
+++ src/server.js
@@ -18,14 +18,48 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const io = SocketIO(server);
 
-io.on("connection", (socket)=>{
-    socket.on("enter_room", (msg,done)=>{ 
-        console.log(msg); 
-        setTimeout(() => {
-            done();
-        },10000);
+function publicRooms(){
+    const {
+        sockets:{
+            adapter: {sids, rooms}
+        }
+    } = io;
+    const publicRooms = [];
+    rooms.forEach((_,key)=>{
+        if(sids.get(key) === undefined ){
+            publicRooms.push(key);
+        }
     });
-})
+    return publicRooms;
+}
+
+function countRoom(roomName){
+    return io.sockets.adapter.rooms.get(roomName)?.size;
+}
+
+io.on("connection", (socket)=>{
+    socket["nickname"] = "Anon";
+    socket.onAny((event)=>{
+        console.log(`Socket event: ${event}`);
+    });
+    socket.on("enter_room", (roomName,done)=>{ 
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome",socket.nickname,countRoom(roomName));
+        io.sockets.emit("room_change",publicRooms());
+    });
+    socket.on("disconnect",()=>{
+        io.sockets.emit("room_change",publicRooms());
+    });
+    socket.on("disconnecting",()=>{
+        socket.rooms.forEach((room) => socket.to(room).emit("bye",socket.nickname,countRoom(room)-1));
+    });
+    socket.on("new_message",(msg,room,done)=>{
+        socket.to(room).emit("new_message",`${socket.nickname}: ${msg}`);
+        done();
+    });
+    socket.on("nickname", (nickname) => socket["nickname"] = nickname);
+});
 
 // const sockets = [];
 
